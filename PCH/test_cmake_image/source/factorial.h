4cb6e619e1cf34f2011432eec5e35113a991f743
Whisperity <whisperity@gmail.com>
2018-07-02 09:45:34 +0200

    Add ccache test image with Cotire example

---
+++ PCH/test_cmake_image/source/factorial.h
@@ -0,0 +1,16 @@
+#ifndef FACTORIAL
+#define FACTORIAL
+
+template <int N>
+struct Factorial 
+{
+    enum { value = N * Factorial<N - 1>::value };
+};
+
+template <>
+struct Factorial<0> 
+{
+    enum { value = 1 };
+};
+
+#endif // FACTORIAL
---
+++ PCH/test_cmake_image/source/header.h
@@ -0,0 +1,6 @@
+#ifndef HEADER
+#define HEADER
+
+int f();
+
+#endif // HEADER
