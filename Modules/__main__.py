25ee941d66c1bb8aa4c0e00622fe2fa282f6b8f4
Whisperity <whisperity@gmail.com>
2019-01-23 14:13:55 +0100

    [NFC] Add some 'argparse' CLI parametrisation of the tool

--- Modules/__main__.py
+++ Modules/__main__.py
@@ -6,6 +6,7 @@ Return codes:
  2 - configuration error.
 """
 
+import argparse
 import os
 import re
 import sys
@@ -14,43 +15,121 @@ import utils
 from utils.graph import nx
 from passes import PassLoader
 
-PassLoader.register_global(
-  'HEADER_FILE_REGEX',
-  re.compile(r'\.(H(XX|PP|\+\+)?|h(xx|pp|\+\+)?|t(xx|pp|\+\+))$'))
+# ------------------- Set up the command-line configuration -------------------
+
+PARSER = argparse.ArgumentParser(
+  prog='automodules',
+  formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+  description="Attempt to perform an automatic modularisation of the given "
+              "C++ project. This automatic modularisation changes input files "
+              "and build relations, but does not - apart from simple renames "
+              "- touch the actual source code.",
+  epilog="There are certain steps that must be made BEFORE this tool can "
+         "successfully run. Such actions include preparing the build system "
+         "of the affected project and generating a build configuration of the "
+         "non-modularised build is necessary.")
+
+PARSER.add_argument('compilation_database',
+                    type=str,
+                    help="Path to the compilation database of the project.")
+PARSER.add_argument('modulescript',
+                    type=str,
+                    help="Path to the location where the Module Maker's CMake "
+                         "extension could should be unpacked to. THIS FILE "
+                         "WILL BE OVERWRITTEN!")
+
+PARSER.add_argument('--symbol-rewriter',
+                    type=str,
+                    metavar='SymbolRewriter',
+                    default='SymbolRewriter',
+                    help="Override path to the 'SymbolRewriter' binary which "
+                         "is used to analyse the project. This binary is "
+                         "mandatory for success and is shipped with the "
+                         "Python driver. (The binary is searched in the PATH "
+                         "environment variable if not overridden.)")
+
+CONFIGS = PARSER.add_argument_group('fine-tune configuration arguments')
+
+
+def _regex_type(s):
+  try:
+    return re.compile(s)
+  except Exception:
+    raise argparse.ArgumentTypeError
+
+
+CONFIGS.add_argument(
+  '--header-regex',
+  type=_regex_type,
+  metavar='REGEX',
+  default=r'\.(H(H|XX|PP|\+\+)?|h(h|xx|pp|\+\+)?|t(t|xx|pp|\+\+))$',
+  help="Regular exception to match for the extension of HEADER / INTERFACE "
+       "files of the project. By default matches hh, hpp/tpp and their xx/++ "
+       "variants.")
+
+CONFIGS.add_argument(
+  '--source-file-regex',
+  type=_regex_type,
+  metavar='REGEX',
+  default=r'\.(C(C|XX|PP|\+\+)?|c(c|xx|pp|\+\+)?|i(i|xx|pp|\+\+))$',
+  help="Regular exception to match for the extension of SOURCE / "
+       "IMPLEMENTATION files of the project. By default matches cc, cpp/ipp "
+       "and their xx/++ variants.")
+
+# TODO: Support specifying -I directives either here, or load them from CDB.
+
+LOGGING = PARSER.add_argument_group('output verbosity arguments')
+
+LOGGING.add_argument('--hide-compiler',
+                     action='store_true',
+                     help="Hide the output of the compiler.")
+
+LOGGING.add_argument('--hide-nonessential',
+                     action='store_true',
+                     help="Hide warning of the tool that are not essential "
+                          "and in many cases can safely be ignored. These "
+                          "warnings are sometimes normal, sometimes expected "
+                          "depending on how the initial input is configured.")
+
+LOGGING.add_argument('--verbose',
+                     action='store_true',
+                     help="Show more verbose status messages about the steps "
+                          "the algorithm took.")
+
+ARGS = PARSER.parse_args()
+
+print(ARGS)
 
 # ---------------------- Sanity check invocation of tool ----------------------
 
-if len(sys.argv) != 3:
-  print("Error: Please specify a 'compile_commands.json' file for the "
-        "project! (As 1st argument.)", file=sys.stderr)
-  print("Error: Please specify the 'Modules.cmake' deploy's location for the "
-        "project! (As 2nd argument.)", file=sys.stderr)
-  sys.exit(2)
+PassLoader.register_global('COMPILE_COMMAND_JSON',
+                           os.path.abspath(ARGS.compilation_database))
 
-COMPILE_COMMAND_JSON = os.path.abspath(sys.argv[1])
-PassLoader.register_global('COMPILE_COMMAND_JSON', COMPILE_COMMAND_JSON)
+PassLoader.register_global('MODULES_CMAKE_SCRIPT',
+                           os.path.abspath(ARGS.modulescript))
 
-MODULES_CMAKE_SCRIPT = os.path.abspath(sys.argv[2])
-PassLoader.register_global('MODULES_CMAKE_SCRIPT', MODULES_CMAKE_SCRIPT)
-
-START_FOLDER = os.getcwd()
-PassLoader.register_global('START_FOLDER', START_FOLDER)
+PassLoader.register_global('START_FOLDER', os.getcwd())
 if not os.path.isfile("CMakeLists.txt"):
-  print("Error: this script should be run from a source folder.",
+  print("Error: this script should be run from the source tree.",
         file=sys.stderr)
   sys.exit(2)
 
-SYMBOL_REWRITER_BINARY = 'SymbolRewriter'
+SYMBOL_REWRITER_BINARY = ARGS.symbol_rewriter
 PassLoader.register_global('SYMBOL_REWRITER_BINARY', SYMBOL_REWRITER_BINARY)
 success, _, _ = utils.call_process(SYMBOL_REWRITER_BINARY, ['--version'])
 if not success:
-  print("The 'SymbolRewriter' binary was not found in the PATH variable. "
-        "This tool is shipped with the Python script and is a required "
-        "dependency.",
+  print("The 'SymbolRewriter' binary was not found in the PATH environment "
+        "directories. This tool is shipped with the Python script and is a "
+        "required dependency.\nBuild the tool and add its output folder to "
+        "PATH, or specify '--symbol-rewriter' manually at invocation.",
         file=sys.stderr)
   sys.exit(2)
 
 # ------------------------- Real execution begins now -------------------------
+
+PassLoader.register_global('LOGGING', {'compiler': not ARGS.hide_compiler,
+                                       'essential': not ARGS.hide_nonessential,
+                                       'verbose': ARGS.verbose})
 
 PassLoader.execute_pass('execute_symbol_rewriter')
 
@@ -70,9 +149,9 @@ PassLoader.register_global('DEFINITIONS', DEFINITIONS)
 PassLoader.register_global('FORWARD_DECLARATIONS', FORWARD_DECLARATIONS)
 
 # Fetch the dependencies from the headers only.
-PassLoader.register_global(
-  'FILTER_FILE_REGEX',
-  PassLoader.get('HEADER_FILE_REGEX'))
+PassLoader.register_global('HEADER_FILE_REGEX', ARGS.header_regex)
+PassLoader.register_global('FILTER_FILE_REGEX',
+                           PassLoader.get('HEADER_FILE_REGEX'))
 PassLoader.execute_pass('fetch_dependency_includes')
 PassLoader.register_global('FILTER_FILE_REGEX', None)
 
@@ -82,9 +161,7 @@ PassLoader.execute_pass('move_implementation_files_to_new_modules')
 
 # After the types had been broken up, implementation files can still have some
 # dependent headers.
-PassLoader.register_global(
-  'FILTER_FILE_REGEX',
-  re.compile(r'\.(C(XX|PP|\+\+)?|c(xx|pp|\+\+)?|i(xx|pp|\+\+))$'))
+PassLoader.register_global('FILTER_FILE_REGEX', ARGS.source_file_regex)
 PassLoader.execute_pass('fetch_dependency_includes')
 PassLoader.register_global('FILTER_FILE_REGEX', None)
 
