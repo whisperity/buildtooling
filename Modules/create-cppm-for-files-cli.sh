9458fd8247c71b784f104ca414b60b38e28df3c4
Whisperity <whisperity@gmail.com>
2022-04-04 14:23:19 +0200

    feat: Parametric CPPM creator from list of files in CLI

--- Modules/create-cppm-for-files-cli.sh
+++ Modules/create-cppm-for-files-cli.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
-echo "#define MODULE_EXPORT" > module.cppm
-echo "" >> module.cppm
-echo "export module FULL_NAME_$1;" >> module.cppm
-shift 1
+MODULE_NAME=$1
+MODULE_FILE=$2
 
-echo -e "\n/* Header files */\n" >> module.cppm
+shift 2
+
+echo "#define MODULE_EXPORT" > $MODULE_FILE.cppm
+echo "" >> $MODULE_FILE.cppm
+echo "export module FULL_NAME_$MODULE_NAME;" >> $MODULE_FILE.cppm
+
+echo -e "\n/* Header files */\n" >> $MODULE_FILE.cppm
 
 echo $@ \
   | tr ' ' '\n' \
   | grep -E "\.(h|hpp|hxx|hh)$" \
   | sed -s 's/^/#include "/g' \
-  | sed -s 's/$/"/g' >> module.cppm
+  | sed -s 's/$/"/g' >> $MODULE_FILE.cppm
 
-echo -e "\n/* Source files */\n" >> module.cppm
+echo -e "\n/* Source files */\n" >> $MODULE_FILE.cppm
 
 echo $@ \
   | tr ' ' '\n' \
   | grep -E "\.(c|cpp|cxx|cc|i|ipp|ixx|ii)$" \
   | sed -s 's/^/#include "/g' \
-  | sed -s 's/$/"/g' >> module.cppm
+  | sed -s 's/$/"/g' >> $MODULE_FILE.cppm
